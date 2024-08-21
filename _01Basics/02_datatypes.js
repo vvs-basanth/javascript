3a37137a2784a385a217ddf18b851b17420a070d
VVS Basanth Pedapati <vvs.pedapati@hotmail.com>
2024-08-21 23:11:03 +0530

    datatypes

---
+++ _01Basics/02_datatypes.js
@@ -0,0 +1,23 @@
+// code readability should be high
+"use strict"; //universal latest version of js.
+
+console.log ("VVS Basanth Pedapati");
+
+let name = "VVS Basanth";
+let age = "23";
+let isLoggedIn = "false";
+let state;
+
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => uinque
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
