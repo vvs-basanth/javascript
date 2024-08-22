8e852424140c273305d373689e94d5a501fe3883
VVS Basanth Pedapati <vvs.pedapati@hotmail.com>
2024-08-22 23:26:24 +0530

    03_conversionOperations

---
+++ _01Basics/03_conversionOperations.js
@@ -0,0 +1,16 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1; false => 0;
+// 1 => true; 0 => false;
+// "" => false
+// "basanth" => true;
